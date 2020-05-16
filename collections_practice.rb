e46e3dfb4037c3ff79ff7f470790d741e2339bf8
Thanh Pham <tpham0123@gmail.com>
2020-05-16 06:04:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,5 +29,9 @@ def reverse_array(array)
   array.reverse 
 end 
 
-def kesha_maker(array)
-  
+def kesha_maker (array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end 
