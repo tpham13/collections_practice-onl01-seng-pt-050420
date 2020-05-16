5cebc413e00c1811d101eba999e1c7f6750bd0e3
Thanh Pham <tpham0123@gmail.com>
2020-05-16 06:07:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,8 +37,12 @@ def kesha_maker (array)
 end 
 
 
-def find_a(array)
-  
+def find_a (array)
+  array.select do |word|
+    word.chr == "a"
+  end
+end
+
 def sum_array (array)
   array.inject do |sum, num|
     sum + num
