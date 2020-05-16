f8f5f4e246d2c0556d643844f726db6ce2f7bf84
Thanh Pham <tpham0123@gmail.com>
2020-05-16 05:02:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,6 +11,12 @@ def sort_array_desc(array)
 end
 
 def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
+  array[1..2] = array[1..2].sort do |a,b|
+    if a==b || a<b || a>b 
+      1
+    end
+  end
+  array
+end
   
 end 
