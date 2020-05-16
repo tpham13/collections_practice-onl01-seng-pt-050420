89872d35beff9946b1479fcdac9b33fe3ec8b4c3
Thanh Pham <tpham0123@gmail.com>
2020-05-16 05:09:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,8 +10,6 @@ def sort_array_desc(array)
     
 end
 
-def swap_elements(strings)
-  strings[1], strings[2] = strings[2], strings[1]
-  return strings
-end
-  
+def sort_array_char_count(array)
+
+
