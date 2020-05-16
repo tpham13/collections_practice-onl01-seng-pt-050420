30bc1c132fb013123e91c796576ad0b94bf586de
Thanh Pham <tpham0123@gmail.com>
2020-05-16 05:11:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,8 @@ def sort_array_char_count(strings)
   strings.sort {|left, right| left.length <=> right.length}
 end
 
-def swap_elements
-
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
+end
 end 
