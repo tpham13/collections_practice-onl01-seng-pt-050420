af78872757d1c6a2d0485a977e0aa4ac49903c74
Thanh Pham <tpham0123@gmail.com>
2020-05-16 06:08:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,5 +49,12 @@ def sum_array (array)
   end
 end
 
-def add_s(array)
-  
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end  
